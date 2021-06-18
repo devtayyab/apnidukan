8fdaa991ff080dfd28cb9d2b71876c81e5e641f8
muhammadtayyabgujjar <gujjartayyab9494@gmail.com>
2021-06-18 07:49:35 +0500

    frontend

---
+++ src/component/add/addproduct.js
@@ -0,0 +1,31 @@
+import React, {useState} from 'react'
+import {FormControl} from '@material-ui/core'
+export default function Addproduct() {
+
+const [product, setproduct] =useStae({
+
+    
+})
+
+    return (
+        <div>
+            <FormControl>
+  <InputLabel htmlFor="my-input">Email address</InputLabel>
+  <Input id="my-input" aria-describedby="my-helper-text" />
+  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+  <InputLabel htmlFor="my-input">Email address</InputLabel>
+  <Input id="my-input" aria-describedby="my-helper-text" />
+  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+  <InputLabel htmlFor="my-input">Email address</InputLabel>
+  <Input id="my-input" aria-describedby="my-helper-text" />
+  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+  <InputLabel htmlFor="my-input">Email address</InputLabel>
+  <Input id="my-input" aria-describedby="my-helper-text" />
+  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+  <InputLabel htmlFor="my-input">Email address</InputLabel>
+  <Input id="my-input" aria-describedby="my-helper-text" />
+  <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+</FormControl>
+        </div>
+    )
+}
---
+++ src/component/home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Navbar from './navbar/navbar'
+import  Cards  from './display/display'
+export default function Home() {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <Cards></Cards>
+        </div>
+    )
+}
